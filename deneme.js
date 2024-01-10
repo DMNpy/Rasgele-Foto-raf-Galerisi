da4b97f3a6c4d7cea1c3f49ef635fcb67d7558c8
DMN <102384385+DMNpy@users.noreply.github.com>
2024-01-10 19:00:23 +0300

    Update deneme.js

--- deneme.js
+++ deneme.js
@@ -5,3 +5,9 @@ for ( let i = 0; i<15; i++) {
     rasgele.innerHTML += '<img src=https://source.unsplash.com/random/${i + 1}" alt="photo">';
     
 }
+
+var yenile= document.getElementById("yenile")
+
+yenile.addEventListener("click", function() {
+    location.reload();
+})
