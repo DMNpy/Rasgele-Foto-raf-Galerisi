c338c841061eecd1db8715b6a89d1ef2b0b030a6
DMN <102384385+DMNpy@users.noreply.github.com>
2024-01-10 18:49:23 +0300

    Create deneme.js

---
+++ deneme.js
@@ -0,0 +1,7 @@
+var rasgele= document.getElementById("rasgele");
+
+for ( let i = 0; i<15; i++) {
+
+    rasgele.innerHTML += '<img src=https://source.unsplash.com/random/${i + 1}" alt="photo">';
+    
+}
